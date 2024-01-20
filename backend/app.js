99e3427884d234a4187d9fc7ba1b65b19bc3f708
cerea62 <cerea62@yandex.ru>
2024-01-20 13:59:11 +0300

    CORs policy update

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,8 @@ const routerCard = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
-const { corsPolicy } = require('./middlewares/corsPolicy');
+// const { corsPolicy } = require('./middlewares/corsPolicy');
+const cors = require('cors');
 
 const NOT_FOUND = 404;
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
@@ -16,9 +17,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(corsPolicy);
+// app.use(corsPolicy);
 
 app.post('/signup', createUser);
 app.post('/signin', login);
